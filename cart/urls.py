70fea41e1d1af9b136da04df3151a4b8fb4b1ba4
Ethan Lu <96506939+elu139@users.noreply.github.com>
2025-09-20 00:14:41 -0400

    Update urls.py

--- cart/urls.py
+++ cart/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('<int:id>/add/', views.add, name='cart.add'),
     path('clear/', views.clear, name='cart.clear'),
     path('purchase/', views.purchase, name='cart.purchase'),
+    path('customer-feedback/', views.customer_feedback_list, name='cart.customer_feedback'),
+    path('submit-feedback/', views.submit_feedback, name='cart.submit_feedback'),
 ]
