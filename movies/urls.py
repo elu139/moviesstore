cc748537bf6b8bdc08bdcfba1b07c7fadf0f36af
Ethan Lu <96506939+elu139@users.noreply.github.com>
2025-09-20 00:18:47 -0400

    Update urls.py

--- movies/urls.py
+++ movies/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path('<int:id>/review/create/', views.create_review, name='movies.create_review'),
     path('<int:id>/review/<int:review_id>/edit/', views.edit_review, name='movies.edit_review'),
     path('<int:id>/review/<int:review_id>/delete/', views.delete_review, name='movies.delete_review'),
+    path('petitions/', views.petitions_index, name='movies.petitions'),
+    path('petitions/create/', views.create_petition, name='movies.create_petition'),
+    path('petitions/<int:petition_id>/upvote/', views.upvote_petition, name='movies.upvote_petition'),
 ]
