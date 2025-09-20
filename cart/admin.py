f90c6098bbac90b8b57bd5f60568693bf2748149
Ethan Lu <96506939+elu139@users.noreply.github.com>
2025-09-20 00:13:44 -0400

    Update admin.py

--- cart/admin.py
+++ cart/admin.py
@@ -1,5 +1,17 @@
 from django.contrib import admin
-from .models import Order, Item
+from .models import Order, Item, CustomerFeedback
+
+class CustomerFeedbackAdmin(admin.ModelAdmin):
+    list_display = ['display_name', 'order', 'created_at', 'feedback_preview']
+    list_filter = ['created_at']
+    search_fields = ['name', 'feedback_text', 'order__id']
+    readonly_fields = ['created_at']
+    ordering = ['-created_at']
+    
+    def feedback_preview(self, obj):
+        return obj.feedback_text[:50] + "..." if len(obj.feedback_text) > 50 else obj.feedback_text
+    feedback_preview.short_description = "Feedback Preview"
 
 admin.site.register(Order)
 admin.site.register(Item)
+admin.site.register(CustomerFeedback, CustomerFeedbackAdmin)
